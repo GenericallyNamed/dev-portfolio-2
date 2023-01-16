bd08c992cf8e635c50f96f24bb4f1a569ba02656
GenericallyNamed <amshand@umich.edu>
2023-01-15 20:05:37 -0500

    added new tags, underlined footer links

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,13 @@ function hoverGlow(e:React.MouseEvent<HTMLDivElement, MouseEvent>) {
 }
 
 function hoverEnd(e:React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    console.log("hoverEnd");
     var card:any = e.target;
     var card_container:any = card.parentElement;
     card_container.style.zIndex = 1;
     var glow = card.querySelector(".special-glow");
-    card_container.style.transform = `scale(1) rotateX(0) rotateY(0)`;
+    card.style.transform = `scale(1) rotateX(0deg) rotateY(0deg)`;
+    card_container.style.transform = `scale(1) rotateX(0deg) rotateY(0deg)`;
 }
 // export var animationEntities = new Map<string,boolean>();
 
@@ -39,11 +41,12 @@ function Footer() {
         <div className="w-screen h-48 bg-gray-800">
             <div className="backdrop-blur-lg rounded-md border border-blue-700 p-2 ml-auto mr-auto max-w-2xl w-[56rem] bg-blue-900 h-[100%] opacity-100 relative top-[-40px] text-gray-300 self-center justify-self-center place-self-center font-mono">
                 <p><span className="text-green-500">user@server</span>:<span className="text-blue-400">~</span>$ ./footer .</p>
-                    {"this is a footer\n\n"}
+                    {"this is a footer"}
+                    <p>click a link if you dare...</p>
                     <p>&zwnj;</p>
                 <p>
                 <span>
-                    <a href="https://github.com/genericallynamed">{"github"}</a> <a href="https://www.linkedin.com/in/alexshandilis/">{"linkedin"}</a> <a href="https://github.com/GenericallyNamed/dev-portfolio-2">{"source-code"}</a> <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">{"funny"}</a>
+                    <a className="underline" href="https://github.com/genericallynamed">{"github"}</a> <a className="underline" href="https://www.linkedin.com/in/alexshandilis/">{"linkedin"}</a> <a className="underline" href="https://github.com/GenericallyNamed/dev-portfolio-2">{"source-code"}</a> <a className="underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">{"funny"}</a>
                 </span>
                 </p>
             </div>
@@ -51,13 +54,24 @@ function Footer() {
     );
 }
 
-function Card({text, link} : {text:string, link:string}) {
+function Tags({tags}:{tags:{name:string, color:string}[]}) {
+    return (
+        <div className="absolute bottom-0 pointer-events-none flex-row flex ml-5 mb-5">
+        {
+            tags.map((tag:{name:string,color:string}) => (<div style={{background:(tag.color)}} className="pointer-events-none font-mono  rounded-sm p-1 mr-2">{tag.name}</div>))
+        }
+        </div>
+    );
+}
+
+function Card({text, link, tags} : {text:string, link:string, tags:{name:string, color:string}[]}) {
     return (
         <a href={link}>
             <div className=" relative landing-card m-4 w-[100%] h-[100%] cursor-pointer justify-self-center overflow-hidden" onMouseMove={hoverGlow} onMouseLeave={hoverEnd}>
                 <div className="relative border border-[rgb(190,190,190)] shadow-[0_0_20px_0.5px_rgba(255,255,255,0.1)] rounded-md bg-[rgb(25,25,25)] h-full">
                     <div className="transition pointer-events-none special-glow w-64 h-64 bg-[rgb(40,40,40)] blur-3xl absolute top-0 left-0"></div>
                     <h2 className="pointer-events-none Franklin text-4xl font-semibold relative pl-6 pt-5">{text}</h2>
+                    <Tags tags={tags}/>
                 </div>
             </div>
         </a>
@@ -76,7 +90,7 @@ export default function Home() {
         </Head>
         <div id="index-container">
             <div id="index-bg" className="absolute h-screen w-scren bg-black">
-                <div className="absolute z-100 h-screen w-screen" style={{background:"radial-gradient(rgba(0,0,0,0),#080808 90%) no-repeat"}}></div>
+                <div className="absolute z-100 h-screen w-screen" style={{background:"radial-gradient(rgba(0,0,0,0), rgba(0,0,0,0) 55%,#080808 75%) no-repeat"}}></div>
                 <canvas id="special-canvas"></canvas>
             </div>
             <div className="absolute top-12 left-[50%] translate-x-[-50%]">
@@ -87,13 +101,13 @@ export default function Home() {
                     coding / utility / entertainment
                 </h2>
                 <div className="ml-auto mr-auto select-none w-[56rem] h-[1290px] max-w-[95vw] rounded-md grid" style={{gridTemplateColumns:"repeat(auto-fill,minmax(290px,1fr))", gridTemplateRows:"repeat(auto-fill,minmax(240px,1.2fr))", rowGap:"15px", columnGap:"15px"}}>
-                    <Card text="portfolio" link="/"/>
-                    <Card text="a-bot370" link="https://github.com/rramboer/A-Bot"/>
-                    <Card text="pathstar" link="https://pathstar.shandilis.dev"/>
-                    <Card text="modernripple" link="https://github.com/GenericallyNamed/ripple-effect"/>
-                    <Card text="freestuffclub" link="https://freestuffclub.neocities.org"/>
-                    <Card text="missingsandwich2" link="https://www.minecraftmaps.com/adventure-maps/the-missing-sandwich-ii"/>
-                    <Card text="annoyingghosts" link="https://www.youtube.com/watch?v=-4NOqVSOTAM"/>
+                    <Card tags={[{name:"typescript",color:"#007acc"},{name:"tailwind",color:"#52a8ac"},{name:"nextJS",color:"black"}]} text="portfolio" link="/"/>
+                    <Card tags={[{name:"javascript",color:"black"},{name:"mongoDB",color:"green"}]} text="a-bot370" link="https://github.com/rramboer/A-Bot"/>
+                    <Card tags={[{name:"typescript",color:"#007acc"},{name:"mongoDB",color:"green"}]} text="pathstar" link="https://pathstar.shandilis.dev"/>
+                    <Card tags={[{name:"javascript",color:"black"},{name:"css",color:"gray"}]} text="modernripple" link="https://github.com/GenericallyNamed/ripple-effect"/>
+                    <Card tags={[{name:"typescript",color:"#007acc"},{name:"mongoDB",color:"green"}]} text="freestuffclub" link="https://freestuffclub.neocities.org"/>
+                    <Card tags={[{name:"playable",color:"red"},{name:"minecraft",color:"blue"}]} text="missingsandwich2" link="https://www.minecraftmaps.com/adventure-maps/the-missing-sandwich-ii"/>
+                    <Card tags={[{name:"playable",color:"red"},{name:"minecraft",color:"blue"}]} text="annoyingghosts" link="https://www.youtube.com/watch?v=-4NOqVSOTAM"/>
                 </div>
                 <Footer/>
             </div>
